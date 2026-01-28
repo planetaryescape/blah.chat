bb4f74c4a36ea1ed7512dc53d86fc8a6aed9c198
Bhekani Khumalo <bhekanik@gmail.com>
2026-01-28 01:26:49 +0000

    feat(ui): add SVG icons for curated integrations
    24 new icons: Airtable, Buffer, Canva, ClickUp, Figma, Firebase,
    HubSpot, Instagram, Intercom, Jira, LinkedIn, Mailchimp, Make,
    Microsoft Teams, OneDrive, Pipedrive, QuickBooks, Trello, X,
    Xero, YouTube, Zapier, Zendesk, Zoom

---
+++ apps/web/src/components/ui/svgs/pipedrive.tsx
@@ -0,0 +1,16 @@
+import type { SVGProps } from "react";
+
+export function Pipedrive(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      fill="#017737"
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>Pipedrive</title>
+      <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm2.4 6.6c1.656 0 3 1.344 3 3s-1.344 3-3 3h-1.8v3.6c0 .993-.807 1.8-1.8 1.8s-1.8-.807-1.8-1.8V8.4c0-.993.807-1.8 1.8-1.8h3.6zm0 3.6c0-.331-.269-.6-.6-.6h-1.2v1.2h1.2c.331 0 .6-.269.6-.6z" />
+    </svg>
+  );
+}
